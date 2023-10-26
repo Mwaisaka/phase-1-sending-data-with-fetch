e6b79e6a23da9ba7d4f583d6b733afa7da6e4cdf
Frank <frankkashere@gmail.com>
2023-10-26 13:00:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email){
+    const formData={
+        name : name,
+        email : email,
+    };
+    const postData = {
+        method: "POST",
+        headers: {
+                "Content-Type": 'application/json',
+                "Accept": 'application/json',
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch(`http://localhost:3000/users`,postData)
+        .then(response=>response.json())
+        .then(object=>{
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+        })
+        .catch(error=>{
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });       
+}
